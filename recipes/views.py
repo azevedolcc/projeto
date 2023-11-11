6a4359bcc9ebfcb252e0fbe88e4ef19acd409a7d
Azevedolcc <azevedolcc@gmail.com>
2023-11-10 23:06:57 -0300

    Estudo10112023

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,8 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 # Create your views here. 
 def home(request):
-    return render(request, 'recipes/home.html', status=201, context={
+    return render(request, 'recipes/pages/home.html', status=201, context={
         'name': 'Luiz Otávio'
     })
-
-def sobre(request):
-    return HttpResponse('sobre')
-
-def contato(request):
-    return HttpResponse('contato')
