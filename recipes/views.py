01ad650b59eb12159b89ed3c49cb09a7b7340905
Azevedolcc <azevedolcc@gmail.com>
2023-11-20 19:20:13 -0300

    201123Atualizacao

--- recipes/views.py
+++ recipes/views.py
@@ -6,3 +6,8 @@ def home(request):
     return render(request, 'recipes/pages/home.html', status=201, context={
         'name': 'Luiz Otávio'
     })
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', status=201, context={
+        'name': 'Luiz Otávio'
+    })
